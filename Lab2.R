31eae9c72772072916f1d0481f7be4f7cb7e06ca
pdziki84-student <wasza.dzikosc@gmail.com>
2021-01-25 18:44:13 +0100

    commit

---
+++ Lab2.R
@@ -0,0 +1,11 @@
+#install.packages("ahp")
+library(ahp)
+
+setwd("E:/studiaMGR/Analiza Procesów Uczenia/Lab2")
+ahp_file_data <- system.file("extdata", "lab2.ahp", package="ahp")
+lab2Ahp <- Load(ahp_file_data)
+Calculate(lab2Ahp)
+Visualize(lab2Ahp)
+Analyze(lab2Ahp)
+AnalyzeTable(lab2Ahp)
+
